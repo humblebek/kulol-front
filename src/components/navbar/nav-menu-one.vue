a4a418fd95427bca5f489163c7569139916e53af
AkmalNorqobilov976 <norqobilovakmal976@gmail.com>
2025-08-28 18:19:31 +0500

    langs

--- src/components/navbar/nav-menu-one.vue
+++ src/components/navbar/nav-menu-one.vue
@@ -14,9 +14,9 @@
                     <p to="#" class="mdi mdi-translate">{{ currentLang.lang }}<span></span></p>
                     <ul class="sub-menu lg:absolute z-50 lg:top-full lg:left-0 lg:min-w-[150px] lg:invisible lg:transition-all lg:bg-white lg:dark:bg-title lg:py-[15px] lg:pr-[30px]">
                         <li v-for="(langItem, index) in langItems" :key="index" class="cursor-pointer">
-                            <RouterLink>
+                            <a>
                                 <p @click="setLanguage(langItem)">{{ langItem.lang }}</p>
-                            </RouterLink>
+                            </a>
                         </li>
                         
                     </ul>
