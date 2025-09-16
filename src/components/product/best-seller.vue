74e55fb803ce90e0333e1ea003642887d9817870
AkmalNorqobilov976 <norqobilovakmal976@gmail.com>
2025-09-16 12:23:04 +0500

    best seller

--- src/components/product/best-seller.vue
+++ src/components/product/best-seller.vue
@@ -6,6 +6,7 @@
 
             </div>
             <div class="mt-5">
+                {{ safeGet(fifthProduct, 'images') }}
                 <div class="grid lg:grid-cols-2 grid-cols-1 gap-5">
                     <div class="grid md:grid-cols-2 grid-cols-1 gap-5">
                         <div class="" v-for="(item, index) in safeGet(products, 'data', []).slice(0,4)" :key="index">
@@ -24,9 +25,9 @@
                     </div>
                     <div class="grid grid-cols-1">
                         <div class="relative group z-[5] before:absolute card-gradient-overlay before:w-full before:h-full before:top-0 before:left-0 before:opacity-0 before:duration-300 before:z-[5] overflow-hidden hover:before:opacity-100 best-seller-pdct">
-                            <img class="w-full aspect-square transform duration-300 group-hover:scale-110 h-full" :src="safeGet(fifthProduct, 'images.main', null)" alt="product-card"/>
+                            <img class="w-full aspect-square transform duration-300 group-hover:scale-110 h-full" :src="storageUrl(safeGet(fifthProduct, 'images.main', null))" alt="product-card"/>
                             <div class="absolute z-10 top-0 left-0 w-full h-full items-start justify-end flex flex-col p-7">
-                               
+                               <h6 class="sm:text-xl font-normal text-white transition-all group-hover:duration-300 transform translate-y-10 opacity-0 group-hover:translate-y-0 group-hover:opacity-100 text-lg mt-3 md:mt-[15px] !leading-[1.5]"><router-link :to="`/products/${safeGet(fifthProduct, 'id', '')}`">{{safeGet(fifthProduct, `translations.${$i18n.locale}.name`, "")}}</router-link></h6>
                             </div>
                         </div>
                     </div>
